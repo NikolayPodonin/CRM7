2e70edf766b3b724602c260c755d0bfcf5bb853e
NikolayPodonin <podonin.developer@gmail.com>
2017-11-20 17:05:19 +0300

    корректная выдача множества результатов в окне КП

--- Client/Program.cs
+++ Client/Program.cs
@@ -23,7 +23,7 @@ namespace Client
         static void Main(string[] args)
         {
             
-            Management man = new Management();
+            //Management man = new Management();
 
             //var ct = /*man.GetAllUsers().First();*/ man.AddUser(new User(), man.AddAccessGroup(new AccessGroup() { Name = "Admin" }).Id, man.AddContact(new Contact() { FirstName = "Pikkolo" }).Id);
             //for (int i = 0; i < 5; i++)
@@ -42,27 +42,27 @@ namespace Client
             Application app = new Application();
             //app.Run(contr);
             
-            ProductModels pmod = new ProductModels();
+            //ProductModels pmod = new ProductModels();
                         
-            var consol = pmod.AddConsolidation(new Consolidation() { Name = "PTFE+C" });
-            var envir = pmod.AddEnvironment(new CRM7.DataModel.Product.Environment() { Name = "Вода" });
-            var connect = pmod.AddValveConnection(new ValveConnection() { Name = "Приварное" });
-            var valveType = pmod.AddValveType(new ValveType() { Name = "Шар" });
-            var valveSeries = pmod.AddValveSeries(new ValveSeries() { Name = "ШК3" });
-            var bodyMaterialType = pmod.AddMaterialType(new MaterialType() { Name = "НЖ" });
-            var bodyMaterial = pmod.AddMaterial(new Material() { Name = "Нержавеющая сталь 03Х17Н14М3" }, bodyMaterialType.Id);
+            //var consol = pmod.GetAllConsolidations().First() /*pmod.AddConsolidation(new Consolidation() { Name = "PTFE+C" })*/;
+            //var envir = pmod.GetAllEnvironments().First() /*pmod.AddEnvironment(new CRM7.DataModel.Product.Environment() { Name = "Вода" })*/;
+            //var connect = pmod.GetAllValveConnections().First() /*pmod.AddValveConnection(new ValveConnection() { Name = "Приварное" })*/;
+            //var valveType = pmod.GetAllValveTypes().First() /*pmod.AddValveType(new ValveType() { Name = "Шар" })*/;
+            //var valveSeries = pmod.AddValveSeries(new ValveSeries() { Name = "ШК4" });
+            //var bodyMaterialType = pmod.GetAllMaterialTypes().First() /*pmod.AddMaterialType(new MaterialType() { Name = "НЖ" })*/;
+            //var bodyMaterial = pmod.GetAllMaterials().First() /*pmod.AddMaterial(new Material() { Name = "Нержавеющая сталь 03Х17Н14М3" }, bodyMaterialType.Id)*/;
 
-            var manufacType = man.AddCompanyType(new CompanyType() { Name = "GearManufacturer", Description = "Производитель электроприводов." });
-            var cont = /*man.GetAllContacts().First()*/ man.AddContact(new Contact() { FirstName = "Николай", LastName="Подонин" });
-            var manufac = man.AddCompany(new Company() { Name = "Auma" }, cont.Id, manufacType.Id);
-            var valveMod = /*pmod.GetAllValveModels().First()*/ pmod.AddValveModel(new ValveModel() { DN = "125/100", PN = 2.5 }, consol.Id, envir.Id, valveType.Id, valveSeries.Id, connect.Id, bodyMaterial.Id, manufac.Id);
-            var rotorType = pmod.AddRotorType(new RotorType() { Name = "Электропривод" });
-            var EAMod = pmod.AddElectricActuatorModel(new ElectricActuatorModel() { Name = "SQ-200" }, manufac.Id, rotorType.Id);
-            var valveRotDismatch = pmod.AddValveRotorDismatch(valveMod.Id, EAMod.Id);
+            //var manufacType = man.AddCompanyType(new CompanyType() { Name = "GearManufacturer", Description = "Производитель электроприводов." });
+            //var cont = man.GetAllContacts().First() /*man.AddContact(new Contact() { FirstName = "Николай", LastName="Подонин" })*/;
+            //var manufac = man.GetAllCompanies().First() /*man.AddCompany(new Company() { Name = "Auma" }, cont.Id, manufacType.Id)*/;
+            //var valveMod = /*pmod.GetAllValveModels().First()*/ pmod.AddValveModel(new ValveModel() { DN = "125/100", PN = 2.5 }, consol.Id, envir.Id, valveType.Id, valveSeries.Id, connect.Id, bodyMaterial.Id, manufac.Id);
+            //var rotorType = pmod.AddRotorType(new RotorType() { Name = "Электропривод" });
+            //var EAMod = pmod.AddElectricActuatorModel(new ElectricActuatorModel() { Name = "SQ-200" }, manufac.Id, rotorType.Id);
+            //var valveRotDismatch = pmod.AddValveRotorDismatch(valveMod.Id, EAMod.Id);
 
-            Catalogue cat = new Catalogue();
-            var catal = /*cat.GetAllCatalogs().First()*/ cat.AddCatalog(new CRM7.DataModel.Catalog.Catalog());
-            cat.AddValveInCatalog(new CRM7.DataModel.Catalog.CatalogPosition.ValveInCatalog(), catal.Id, valveMod.Id);
+            //Catalogue cat = new Catalogue();
+            //var catal = /*cat.GetAllCatalogs().First()*/ cat.AddCatalog(new CRM7.DataModel.Catalog.Catalog());
+            //cat.AddValveInCatalog(new CRM7.DataModel.Catalog.CatalogPosition.ValveInCatalog(), catal.Id, valveMod.Id);
 
 
             WPF.CPForms.WindowMainCP CP = new WPF.CPForms.WindowMainCP();
--- Client/WPF/CPForms/WindowMainCP.xaml.cs
+++ Client/WPF/CPForms/WindowMainCP.xaml.cs
@@ -42,139 +42,173 @@ namespace Client.WPF.CPForms
             //Здесь мы обращаемся на сервер каждый раз, чтобы получить характеристики моделей арматуры, которые отобразятся в окне.
             lbx_Manufacturer.SelectionChanged += (s, e) =>
             {
-                lbx_Type.Items.Clear();
-                foreach ( var valveType in cat.GetValveTypesOfManufacturerInCatalog(catalog.Id, ((Company)lbx_Manufacturer.SelectedItem).Id))
+                if(lbx_Manufacturer.SelectedItem != null)
                 {
-                    lbx_Type.Items.Add(valveType);
+                    lbx_Type.Items.Clear();
+                    foreach (var valveType in cat.GetValveTypesOfManufacturerInCatalog(catalog.Id, ((Company)lbx_Manufacturer.SelectedItem).Id))
+                    {
+                        lbx_Type.Items.Add(valveType);
+                    }
+                    lbx_Type.SelectedIndex = 0;
                 }
-                lbx_Type.SelectedIndex = 0;
             };
             lbx_Type.SelectionChanged += (s, e) =>
             {
-                lbx_Series.Items.Clear();
-                foreach (var series in cat.GetValveSeriesOfTypeInCatalog(catalog.Id, ((Company)lbx_Manufacturer.SelectedItem).Id, ((ValveType)lbx_Type.SelectedItem).Id))
+                if(lbx_Type.SelectedItem != null)
                 {
-                    lbx_Series.Items.Add(series);
+                    lbx_Series.Items.Clear();
+                    foreach (var series in cat.GetValveSeriesOfTypeInCatalog(catalog.Id, ((Company)lbx_Manufacturer.SelectedItem).Id, ((ValveType)lbx_Type.SelectedItem).Id))
+                    {
+                        lbx_Series.Items.Add(series);
+                    }
+                    lbx_Series.SelectedIndex = 0;
                 }
-                lbx_Series.SelectedIndex = 0;
             };
             lbx_Series.SelectionChanged += (s, e) =>
             {
-                lbx_Connection.Items.Clear();
-                foreach (var connect in cat.GetValveConnectionOfSeriesInCatalog(catalog.Id, 
-                    ((Company)lbx_Manufacturer.SelectedItem).Id, 
-                    ((ValveType)lbx_Type.SelectedItem).Id, 
-                    ((ValveSeries)lbx_Series.SelectedItem).Id))
+                if(lbx_Series.SelectedItem != null)
                 {
-                    lbx_Connection.Items.Add(connect);
+                    lbx_Connection.Items.Clear();
+                    foreach (var connect in cat.GetValveConnectionOfSeriesInCatalog(catalog.Id,
+                        ((Company)lbx_Manufacturer.SelectedItem).Id,
+                        ((ValveType)lbx_Type.SelectedItem).Id,
+                        ((ValveSeries)lbx_Series.SelectedItem).Id))
+                    {
+                        lbx_Connection.Items.Add(connect);
+                    }
+                    lbx_Connection.SelectedIndex = 0;
                 }
-                lbx_Connection.SelectedIndex = 0;
             };
             lbx_Connection.SelectionChanged += (s, e) =>
             {
-                lbx_Environment.Items.Clear();
-                foreach (var envir in cat.GetValveEnvironmentOfConnectionInCatalog(catalog.Id,
-                    ((Company)lbx_Manufacturer.SelectedItem).Id,
-                    ((ValveType)lbx_Type.SelectedItem).Id,
-                    ((ValveSeries)lbx_Series.SelectedItem).Id,
-                    ((ValveConnection)lbx_Connection.SelectedItem).Id))
+                if(lbx_Connection.SelectedItem != null)
                 {
-                    lbx_Environment.Items.Add(envir);
+                    lbx_Environment.Items.Clear();
+                    foreach (var envir in cat.GetValveEnvironmentOfConnectionInCatalog(catalog.Id,
+                        ((Company)lbx_Manufacturer.SelectedItem).Id,
+                        ((ValveType)lbx_Type.SelectedItem).Id,
+                        ((ValveSeries)lbx_Series.SelectedItem).Id,
+                        ((ValveConnection)lbx_Connection.SelectedItem).Id))
+                    {
+                        lbx_Environment.Items.Add(envir);
+                    }
+                    lbx_Environment.SelectedIndex = 0;
                 }
-                lbx_Environment.SelectedIndex = 0;
             };
             lbx_Environment.SelectionChanged += (s, e) =>
             {
-                lbx_MaterialBody.Items.Clear();
-                foreach (var mb in cat.GetValveMaterialBodyOfEnvironmentInCatalog(catalog.Id,
-                    ((Company)lbx_Manufacturer.SelectedItem).Id,
-                    ((ValveType)lbx_Type.SelectedItem).Id,
-                    ((ValveSeries)lbx_Series.SelectedItem).Id,
-                    ((ValveConnection)lbx_Connection.SelectedItem).Id,
-                    ((CRM7.DataModel.Product.Environment)lbx_Environment.SelectedItem).Id))
+                if(lbx_Environment.SelectedItem != null)
                 {
-                    lbx_MaterialBody.Items.Add(mb);
+                    lbx_MaterialBody.Items.Clear();
+                    foreach (var mb in cat.GetValveMaterialBodyOfEnvironmentInCatalog(catalog.Id,
+                        ((Company)lbx_Manufacturer.SelectedItem).Id,
+                        ((ValveType)lbx_Type.SelectedItem).Id,
+                        ((ValveSeries)lbx_Series.SelectedItem).Id,
+                        ((ValveConnection)lbx_Connection.SelectedItem).Id,
+                        ((CRM7.DataModel.Product.Environment)lbx_Environment.SelectedItem).Id))
+                    {
+                        lbx_MaterialBody.Items.Add(mb);
+                    }
+                    lbx_MaterialBody.SelectedIndex = 0;
                 }
-                lbx_MaterialBody.SelectedIndex = 0;
             };
             lbx_MaterialBody.SelectionChanged += (s, e) =>
             {
-                lbx_Consolidation.Items.Clear();
-                foreach (var mb in cat.GetValveConsolidationOfMaterialInCatalog(catalog.Id,
-                    ((Company)lbx_Manufacturer.SelectedItem).Id,
-                    ((ValveType)lbx_Type.SelectedItem).Id,
-                    ((ValveSeries)lbx_Series.SelectedItem).Id,
-                    ((ValveConnection)lbx_Connection.SelectedItem).Id,
-                    ((CRM7.DataModel.Product.Environment)lbx_Environment.SelectedItem).Id,
-                    ((Material)lbx_MaterialBody.SelectedItem).Id))
+                if(lbx_MaterialBody.SelectedItem != null)
                 {
-                    lbx_Consolidation.Items.Add(mb);
+                    lbx_Consolidation.Items.Clear();
+                    foreach (var mb in cat.GetValveConsolidationOfMaterialInCatalog(catalog.Id,
+                        ((Company)lbx_Manufacturer.SelectedItem).Id,
+                        ((ValveType)lbx_Type.SelectedItem).Id,
+                        ((ValveSeries)lbx_Series.SelectedItem).Id,
+                        ((ValveConnection)lbx_Connection.SelectedItem).Id,
+                        ((CRM7.DataModel.Product.Environment)lbx_Environment.SelectedItem).Id,
+                        ((Material)lbx_MaterialBody.SelectedItem).Id))
+                    {
+                        lbx_Consolidation.Items.Add(mb);
+                    }
+                    lbx_Consolidation.SelectedIndex = 0;
                 }
-                lbx_Consolidation.SelectedIndex = 0;
             };
             //Здесь мы уже получаем не характеристики, а сами модели арматуры.
             lbx_Consolidation.SelectionChanged += (s, e) =>
             {
-                lbx_Controlling.Items.Clear();
-                foreach (var vm in cat.GetValveModelsOfConsolidationInCatalog(catalog.Id,
-                    ((Company)lbx_Manufacturer.SelectedItem).Id,
-                    ((ValveType)lbx_Type.SelectedItem).Id,
-                    ((ValveSeries)lbx_Series.SelectedItem).Id,
-                    ((ValveConnection)lbx_Connection.SelectedItem).Id,
-                    ((CRM7.DataModel.Product.Environment)lbx_Environment.SelectedItem).Id,
-                    ((Material)lbx_MaterialBody.SelectedItem).Id,
-                    ((Consolidation)lbx_Consolidation.SelectedItem).Id))
+                if (lbx_Consolidation.SelectedItem != null)
                 {
-                    lbx_Controlling.Items.Add(vm);
-                    valveModels.Add(vm);
+                    lbx_Controlling.Items.Clear();
+                    valveModels.Clear();
+                    foreach (var vm in cat.GetValveModelsOfConsolidationInCatalog(catalog.Id,
+                        ((Company)lbx_Manufacturer.SelectedItem).Id,
+                        ((ValveType)lbx_Type.SelectedItem).Id,
+                        ((ValveSeries)lbx_Series.SelectedItem).Id,
+                        ((ValveConnection)lbx_Connection.SelectedItem).Id,
+                        ((CRM7.DataModel.Product.Environment)lbx_Environment.SelectedItem).Id,
+                        ((Material)lbx_MaterialBody.SelectedItem).Id,
+                        ((Consolidation)lbx_Consolidation.SelectedItem).Id))
+                    {
+                        lbx_Controlling.Items.Add(vm);
+                        valveModels.Add(vm);
+                    }
+                    lbx_Controlling.SelectedIndex = 0;
                 }
-                lbx_Controlling.SelectedIndex = 0;
             };
             // Дальше к элементам окна привязываются уже не характеристики арматуры, а сохраненные в клиенте модели арматуры, но отображаются только их характеристики.
             lbx_Controlling.SelectionChanged += (s, e) =>
             {
-                lbx_DN.Items.Clear();
-                foreach (var vm in valveModels.Where(i => i.Controlling == ((ValveModel)lbx_Controlling.SelectedItem).Controlling).GroupBy(i => i.DN).Select(gr => gr.First()))
+                if(lbx_Controlling.SelectedItem != null)
                 {
-                    lbx_DN.Items.Add(vm);
-                }
-                lbx_DN.SelectedIndex = 0;
+                    lbx_DN.Items.Clear();
+                    foreach (var vm in valveModels.Where(i => i.Controlling == ((ValveModel)lbx_Controlling.SelectedItem).Controlling).GroupBy(i => i.DN).Select(gr => gr.First()))
+                    {
+                        lbx_DN.Items.Add(vm);
+                    }
+                    lbx_DN.SelectedIndex = 0;
+                }                
             };
             lbx_DN.SelectionChanged += (s, e) =>
             {
-                lbx_PN.Items.Clear();
-                foreach (var vm in valveModels.Where(i => i.DN == ((ValveModel)lbx_DN.SelectedItem).DN).GroupBy(i => i.PN).Select(gr => gr.First()))
+                if(lbx_DN.SelectedItem != null)
                 {
-                    lbx_PN.Items.Add(vm);
-                }
-                lbx_PN.SelectedIndex = 0;
+                    lbx_PN.Items.Clear();
+                    foreach (var vm in valveModels.Where(i => i.DN == ((ValveModel)lbx_DN.SelectedItem).DN).GroupBy(i => i.PN).Select(gr => gr.First()))
+                    {
+                        lbx_PN.Items.Add(vm);
+                    }
+                    lbx_PN.SelectedIndex = 0;
+                }                
             };
-            // Отсюда начинаются другие элементы, модель арматуры мы уже выбрали.
+            //Отсюда начинаются другие элементы, модель арматуры мы уже выбрали.
             lbx_PN.SelectionChanged += (s, e) =>
             {
-                lbx_RotorType.Items.Clear();
-                foreach (var rt in valveModels.First(i => i.PN == ((ValveModel)lbx_PN.SelectedItem).PN).RotorDismatches.Select(rm => rm.RotorModel.Type))
+                if(lbx_PN.SelectedItem != null)
                 {
-                    lbx_RotorType.Items.Add(rt);
-                }
-                lbx_RotorType.SelectedIndex = 0;
+                    lbx_RotorType.Items.Clear();
+                    foreach (var rt in valveModels.First(i => i.PN == ((ValveModel)lbx_PN.SelectedItem).PN).RotorDismatches.Select(rm => rm.RotorModel.Type))
+                    {
+                        lbx_RotorType.Items.Add(rt);
+                    }
+                    lbx_RotorType.SelectedIndex = 0;
+                }                
             };
             lbx_RotorType.SelectionChanged += (s, e) =>
             {
-                lbx_Rotors.Items.Clear();
-                //условие надо будет изменить на не содержащее строковых сравнений. 
-                if (((RotorType)lbx_RotorType.SelectedItem).Name != "Голый шток" && ((RotorType)lbx_RotorType.SelectedItem).Name != "Ручной рычаг")
+                if (lbx_RotorType.SelectedItem != null)
                 {
-                    lbx_Rotors.IsEnabled = true;
-                    foreach(var rm in ((ValveModel)lbx_PN.SelectedItem).RotorDismatches.Select(rd => rd.RotorModel).Where(rm => rm.TypeId == ((RotorType)lbx_RotorType.SelectedItem).Id))
+                    lbx_Rotors.Items.Clear();
+                    //условие надо будет изменить на не содержащее строковых сравнений. 
+                    if (((RotorType)lbx_RotorType.SelectedItem).Name != "Голый шток" && ((RotorType)lbx_RotorType.SelectedItem).Name != "Ручной рычаг")
                     {
-                        lbx_Rotors.Items.Add(rm);
+                        lbx_Rotors.IsEnabled = true;
+                        foreach (var rm in ((ValveModel)lbx_PN.SelectedItem).RotorDismatches.Select(rd => rd.RotorModel).Where(rm => rm.TypeId == ((RotorType)lbx_RotorType.SelectedItem).Id))
+                        {
+                            lbx_Rotors.Items.Add(rm);
+                        }
                     }
-                }
-                else
-                {
-                    lbx_Rotors.IsEnabled = false;
-                }
+                    else
+                    {
+                        lbx_Rotors.IsEnabled = false;
+                    }
+                }                    
             };
 
             foreach (var company in cat.GetValveManufacturersInCatalog(catalog.Id))
